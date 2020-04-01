2d15abb6a18e5b9e357ccf40edd2bce5093589f7
neha <nehatyagi2516@gmail.com>
2020-04-01 14:03:48 +0530

    Successful Committed

---
+++ src/main/java/com/rtpl/common/Customer.java
@@ -0,0 +1,24 @@
+package com.rtpl.common;
+
+import java.util.Map;
+
+public class Customer {
+
+	private Map maps;
+
+	public Map getMaps() {
+		return maps;
+	}
+
+	public void setMaps(Map maps) {
+		this.maps = maps;
+	}
+
+	@Override
+	public String toString() {
+		return "Customer [maps=" + maps + "]";
+	}
+
+	
+}
+
